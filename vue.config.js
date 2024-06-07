01955b2f2ab39d08d8da2cecda959835f5f64e4c
mlbboy1124 <43491856+mlbboy1124@users.noreply.github.com>
2024-06-07 20:26:20 +0900

    update DB issue v5

--- vue.config.js
+++ vue.config.js
@@ -18,7 +18,7 @@ module.exports = {
   devServer: {
     proxy: {
       "/api": {
-        target: "http://backend:3000",
+        target: "https://port-0-backend-and8yv2klx4clqjs.sel5.cloudtype.app/",
         changeOrigin: true,
         secure: false,
       },
