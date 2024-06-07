2cdd91465c6c9d930f49e58f09b76c7496879daf
mlbboy1124 <43491856+mlbboy1124@users.noreply.github.com>
2024-06-07 22:54:32 +0900

    update refresh issue v2

--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <header class="d-flex flex-wrap justify-content-between align-items-center py-3 mb-4 border-bottom">
-      <a href="/home" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+      <a href="/#/home" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
         <img src="@/assets/logo.png" class="headLogo">
         <span class="logo-item">S&K법무사사무소</span>
       </a>
