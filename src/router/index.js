d1fc8af9d813650221f29427148913aa07b796f7
mlbboy1124 <43491856+mlbboy1124@users.noreply.github.com>
2024-06-10 12:05:58 +0900

    update refresh issue

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '@/components/HomePage.vue';
 
 import CoReg_Page from '@/components/Co-Reg/CoReg_Page.vue';
@@ -31,7 +31,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 });
 
