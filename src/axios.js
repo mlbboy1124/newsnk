e346a8ecae082ca25593a42ab708d9e67e40b4d9
mlbboy1124 <43491856+mlbboy1124@users.noreply.github.com>
2024-06-11 11:58:55 +0900

    update auth v2

--- src/axios.js
+++ src/axios.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 const instance = axios.create({
   // 클라우드 접속
-  // baseURL: 'https://port-0-backend-and8yv2klx4clqjs.sel5.cloudtype.app'
+  baseURL: 'https://port-0-backend-and8yv2klx4clqjs.sel5.cloudtype.app'
   // //로컬 접속
-  baseURL: 'http://localhost:3000'
+  // baseURL: 'http://localhost:3000'
 });
 
 instance.interceptors.request.use(config => {
