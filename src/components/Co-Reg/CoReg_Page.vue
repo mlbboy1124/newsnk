a079d176c4ca71106260dfcb29aefa18806c3887
mlbboy1124 <43491856+mlbboy1124@users.noreply.github.com>
2024-07-25 09:40:12 +0900

    debug v7

--- src/components/Co-Reg/CoReg_Page.vue
+++ src/components/Co-Reg/CoReg_Page.vue
@@ -12,10 +12,12 @@
   </div>
 </template> -->
 <template>
+    <div class="container">
   <div class="CoReg_submit-container">
     <h3>상업등기 신청 페이지 제작중</h3>
     <p>상업등기 신청 페이지는 제작중 입니다.</p>
   </div>
+</div>
 </template>
 
 <script>
